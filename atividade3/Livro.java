187a109180400f0b742623e346e557a46827cb8a
NathanGab <151534404+NathanGab@users.noreply.github.com>
2024-08-29 22:12:28 -0300

    Livro.java

--- atividade3/Livro.java
+++ atividade3/Livro.java
@@ -3,7 +3,7 @@ package Exercicio3;
 public class Livro {
 
 	public static void main(String[] args) {
-		LojaDeLivros Livro = new LojaDeLivros("Romeu e Julieta", "William Shakespeare", 1597, 33,00);
+		LojaDeLivros Livro = new LojaDeLivros("Romeu e Julieta", "William Shakespeare", 1597);
 		Livro.imprimirDados();
 	}
 
